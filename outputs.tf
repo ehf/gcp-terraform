dc5e708c68a119b98bab2238b0dd2b632f89ff82
ehf <1011013+ehf@users.noreply.github.com>
2021-11-23 20:08:44 -0800

    output name of instances along with IP

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,9 @@
 // A variable for extracting the external IP address of the instance
 //value = google_compute_instance.default[count.index].network_interface.0.access_config.0.nat_ip
+
+output "name" {
+  value = google_compute_instance.default.*.name
+}
 output "ip" {
   value = google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip
 }
