6f7f03b463716314b77da10d883b595fee2cdd9e
ehf <1011013+ehf@users.noreply.github.com>
2021-11-23 20:24:23 -0800

    output map of instance name to IP

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,10 @@ output "name" {
 output "ip" {
   value = google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip
 }
+
+output "instances" {
+  value = {
+    for instance in google_compute_instance.default:
+        instance.name => instance.network_interface.0.access_config.0.nat_ip
+  }
+}
